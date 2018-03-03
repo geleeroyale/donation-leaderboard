b25a1b760937bfea78ed7c9cbe046b4526e8d3bb
geleeroyale <krassvs@gmail.com>
2018-03-03 10:36:09 +0100

    fixed link

--- src/App.js
+++ src/App.js
@@ -227,12 +227,12 @@ class App extends Component {
       <div className="flex-row d-flex justify-content-around">
         <div className="flex-column introColumn">
         <img src="/img/scalingnow.png" className="typelogo img-fluid"/>
-          <p><a href="https://web3.foundation/">Web3 Foundation</a> and <a href="https//giveth.io">Giveth</a> are hosting an in-person gathering exploring Ethereum Scaling Solutions on <strong>March 5th & 6th in Barcelona</strong>. This DApp acts as donation gateway and attendee list.</p>
+          <p><a href="https://web3.foundation/">Web3 Foundation</a> and <a href="https://giveth.io">Giveth</a> are hosting an in-person gathering exploring Ethereum Scaling Solutions on <strong>March 5th & 6th in Barcelona</strong>. This DApp acts as donation gateway and attendee list.</p>
           <p><strong>March 5th</strong> is an invite-only event for select devs working on immediate scaling solutions to share their insights amongst one another.
           </p>
           <p><strong>March 6th</strong> is open to DApp developers who submit a <a href="https://docs.google.com/forms/d/1tMq8AamiQ2PI0zulo_jxIV4Ef9h0D9DjrgjfSK70I0M/viewform?edit_requested=true">super quick & easy form</a>, each attendee must donate from a <strong>unique</strong> address so that we can count how many Tapas we need as requested in the form .</p>
           <p>We encourage you to open your wallets and let the donations flow through. Your donation covers the costs of venue rentals and food expenses. We aren’t trying to force some cellophane wrapped sad-wiches on you - this is Barcelona, we’ve already found the best Tapas and Cava joints so we can collaborate over real Catalan culture.</p>
-          <p>If we don’t receive enough donations to cover the event, the restaurants will be BYOETH, please be generous, because as of Feb 27th <strong>we are on pace to only cover a small percentage of the cost of the venues :-(</strong></p>
+          <p>If we don’t receive enough donations to cover the event, the restaurants will be BYOETH, <strong>please be generous</strong>, you'll also indirectly sponsor complementary initiaves such as <a href="https://www.youtube.com/watch?v=aWvzQMorof0&list=PL4Artm1rmCWGksgoRe6HF5d9eklC01IcC">[Giveth's ScalingNOW! interview series]</a>.</p>
           <p>The ETH raised will be transparently tracked using the <a href="https://alpha.giveth.io/campaigns/ap6KXg8iJwwUAxBY">Giveth Platform</a> And if any donations are received beyond the costs detailed in the ScalingNow! Giveth Campaign, they will be split equally between Giveth and the Web3 Foundation to help make more magic like this happen.</p>
           <p>Amount donated: <b>{this.state.totalAmount} ETH</b></p>
           </div>
